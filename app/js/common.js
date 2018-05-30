1d6f8e8c4a7e50d065aa6d17c8ef86f42ce62f13
Korobeynik <korobeynik77@gmail.com>
2018-05-30 16:30:19 +0300

    Slider add animation, added popup

--- app/js/common.js
+++ app/js/common.js
@@ -1,14 +1,38 @@
+
+$(document).ready(function(){
+    $('input').blur(function(){
+        //console.log($(this).val().length)
+      if ( $(this).val().length !==  0 ) {
+            $(this).addClass('full');
+      } else {
+        $(this).removeClass('full');
+      }
+    });
+});
+
 (function() {
+
+    $('#c-form').submit(function(event) {
+       event.preventDefault();
+    });
 
     var owl = $('.owl-carousel');
     owl.owlCarousel({
+        animateOut: 'slideOutDown',
+        animateIn: 'fadeIn',
         items:1,
         loop:true,
         margin: 0,
+        smartSpeed: 500,
         autoplay:true,
         autoplayTimeout:5000,
         autoplayHoverPause:true
+    }).on('translate.owl.carousel', function() {
+       $(this).find('.composition-text').removeClass('animated fadeInUp');
+    }).on('translated.owl.carousel', function() {
+       $(this).find('.composition-text').addClass('animated fadeInUp');
     });
+
 
     $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
@@ -103,3 +127,51 @@
     }
 
 })();
+
+
+(function() {
+    var modalEl = document.querySelector('.modal'),
+        revealer = new RevealFx(modalEl),
+        deleteCtrl = modalEl.querySelector('.btn--default'),
+        closeCtrl = modalEl.querySelector('.btn--modal-close');
+
+    document.querySelector('.btn--modal-open').addEventListener('click', function() {
+        modalEl.classList.add('modal--open');
+        revealer.reveal({
+            bgcolor: '#FF633B',
+            direction: 'tb',
+            duration: 400,
+            easing: 'easeOutCirc',
+            onCover: function(contentEl, revealerEl) {
+                contentEl.style.opacity = 1;
+            },
+            onComplete: function() {
+                closeCtrl.addEventListener('click', closeModal);
+                //deleteCtrl.addEventListener('click', closeModal);
+            }
+        });
+    });
+
+    function closeModal(ev) {
+        closeCtrl.removeEventListener('click', closeModal);
+        modalEl.classList.remove('modal--open');
+        revealer.reveal({
+            bgcolor: ev.target.classList.contains('btn--modal-close') ? '#FF633B' : '#40f180',
+            direction: 'bt',
+            duration: ev.target.classList.contains('btn--modal-close') ? 200 : 400,
+            easing: 'easeOutCirc',
+            onCover: function(contentEl, revealerEl) {
+                contentEl.style.opacity = 0;
+            },
+            onComplete: function() {
+                modalEl.classList.remove('modal--open');
+            }
+        });
+    }
+
+    deleteCtrl.addEventListener('submit', function(event){
+        event.preventDefault();
+    });
+
+    })();
+
